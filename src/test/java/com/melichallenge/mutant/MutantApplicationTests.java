40dbfd88f6d378ecdfe178999f0e34601d2a229f
Santiago Rivera R <sriverarestrepo@gmail.com>
2021-04-12 21:51:36 -0500

    Inicializacion de desarrollo API para detectar mutantes

---
+++ src/test/java/com/melichallenge/mutant/MutantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melichallenge.mutant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
